d2e86915633e77ae0efeb86472cf569e41e4dff2
VioletsOleander <1377232072@qq.com>
2026-06-05 16:51:34 +0800

    feat!: support insert surrounding, drop support for insert begin and end

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,23 @@
 import { Plugin } from "obsidian";
-import { insertEnd, insertStart } from "./utils";
+import { insertSurround } from "./utils";
 
 import type { Editor } from "obsidian";
 
 class Vanillian extends Plugin {
   override async onload(): Promise<void> {
     this.addCommand({
-      id: "insert-remark-start",
-      name: "Insert remark start",
+      id: "insert-remark",
+      name: "Insert remark",
       editorCallback: (editor: Editor) => {
-        insertStart(editor, "Remark");
+        insertSurround(editor, "Remark");
       },
     });
 
     this.addCommand({
-      id: "insert-remark-end",
-      name: "Insert remark end",
+      id: "insert-background-konwledge",
+      name: "Insert background knowledge",
       editorCallback: (editor: Editor) => {
-        insertEnd(editor, "Remark");
-      },
-    });
-
-    this.addCommand({
-      id: "insert-background-konwledge-start",
-      name: "Insert background knowledge start",
-      editorCallback: (editor: Editor) => {
-        insertStart(editor, "Background Knowledge");
-      },
-    });
-
-    this.addCommand({
-      id: "insert-background-konwledge-end",
-      name: "Insert background knowledge end",
-      editorCallback: (editor: Editor) => {
-        insertEnd(editor, "Background Knowledge");
+        insertSurround(editor, "Background Knowledge");
       },
     });
   }
